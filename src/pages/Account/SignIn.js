c7969d5ea3a97cc4796bc8d3aa6d37e73eb7edc9
confytech <confiehiemere@gmail.com>
2026-05-04 04:50:28 +0100

    my files

--- src/pages/Account/SignIn.js
+++ src/pages/Account/SignIn.js
@@ -62,7 +62,7 @@ const SignIn = () => {
             </span>
             <p className="text-base text-gray-300">
               <span className="text-white font-semibold font-titleFont">
-                Get started fast with OREBI
+                Get started fast with Ama Retail Store
               </span>
               <br />
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis
@@ -75,7 +75,7 @@ const SignIn = () => {
             </span>
             <p className="text-base text-gray-300">
               <span className="text-white font-semibold font-titleFont">
-                Access all OREBI services
+                Access all Ama Retail Store services
               </span>
               <br />
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis
